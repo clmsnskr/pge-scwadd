685cc7c9b93cbd8f3f6d70865579f557595cd104
tomas.cormons <tcormons@gmail.com>
2022-01-12 15:30:51 -0800

    includes comment to document potential use case for data payload in body of POST request, currently working without it

--- src/index.js
+++ src/index.js
@@ -1,62 +1,62 @@
-const { default: axios } = require("axios");
-const https = require("https");
-const fs = require("fs");
-const express = require("express");
+const { default: axios } = require('axios');
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
 const app = express();
-const port = 3003;
-const qs = require("qs");
+const port = 3004;
 
 const SMD_AUTH_BASE_URL = `https://sharemydataqa.pge.com/myAuthorization/`;
 const PGE_API_BASE_URL = `https://apiqa.pge.com`;
 
-const QA_CLIENT_ID = "<SECRET_STRING>";
-const QA_CLIENT_SECRET = "<SECRET_STRING>";
-const PROD_CLIENT_ID = "<SECRET_STRING>";
-
-const CLIENT_ID = QA_CLIENT_ID;
-const CLIENT_SECRET = QA_CLIENT_SECRET;
+const QA_CLIENT_ID = '<SECRET_STRING>';
+const PROD_CLIENT_ID = '<SECRET_STRING>';
 
 const today = new Date();
 const twoDaysAgo = new Date(today).setDate(today.getDate() - 2);
 const oneYearAgo = new Date(today).setDate(today.getDate() - 365);
 
 const smdAuthParams = {
-  client_id: CLIENT_ID,
-  redirect_uri: "https://www.isharefood.com/OAuthCallback",
-  response_type: "code",
-  login: "guest"
+  client_id: QA_CLIENT_ID,
+  redirect_uri: 'https://www.isharefood.com/OAuthCallback',
+  response_type: 'code',
+  login: 'guest',
+  // scope: `MinAuthEndDate=${twoDaysAgo};PreferredAuthEndDate=${oneYearAgo}`
 };
 
-const withQuery = (params) => (url) =>
-  `${url}${Object.keys(params).length ? "?" : ""}${qs.stringify(params)}`;
+const withQuery = (query) => (url) => {
+  const params = Object.keys(query).reduce(
+    (acc, key, idx) => `${acc}${idx === 0 ? '?' : '&'}${key}=${query[key]}`,
+    ''
+  );
+  return `${url}${params}`;
+};
 
-const encode64 = (str) => Buffer.from(str, "utf-8").toString("base64");
-
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const url = withQuery(smdAuthParams)(SMD_AUTH_BASE_URL);
   res.redirect(url);
 });
 
-app.get("/OAuthCallback", async (req, res, next) => {
+app.get('/OAuthCallback', async (req, res, next) => {
   const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Basic ${encode64(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
+    'Content-Type': 'application/json',
+    Authorization: `Basic <SECRET_STRING>`,
   };
 
   const httpsAgent = new https.Agent({
-    cert: fs.readFileSync("ssl/certs/isharefood.com/certificate.crt"),
-    key: fs.readFileSync("ssl/private/isharefood.com/private.key"),
+    cert: fs.readFileSync('ssl/certs/isharefood.com/certificate.crt'),
+    key: fs.readFileSync('ssl/private/isharefood.com/private.key'),
   });
 
   const data = {
-    grant_type: "authorization_code",
+    grant_type: 'authorization_code',
     code: req.query.code,
-    redirect_uri: "https://www.isharefood.com/OAuthCallback",
+    redirect_uri: 'https://www.isharefood.com/OAuthCallback',
   };
 
   const result = await axios.post(
     withQuery(data)(`https://apiqa.pge.com/datacustodian/oauth/v2/token`),
-    "",
+    // TODO: data payload could be necessary arg, currently works as params above ^^^
+    '',
     { httpsAgent, headers }
   );
   res.send(result.data);
