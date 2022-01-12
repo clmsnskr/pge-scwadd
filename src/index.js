a5a72518eb126280367cd3deb3459a53d2eac312
Chris Coker <chris.coker@verys.com>
2022-01-12 16:46:18 -0500

    initial commit and tested client_credentials call to qa api

---
+++ src/index.js
@@ -0,0 +1,89 @@
+const { default: axios } = require("axios");
+const https = require('https');
+const fs = require('fs');
+const express = require("express");
+const app = express();
+const port = 3003;
+
+const SMD_AUTH_BASE_URL = `https://sharemydataqa.pge.com/myAuthorization/`;
+const PGE_API_BASE_URL = `https://apiqa.pge.com`;
+
+const QA_CLIENT_ID = "9b7c5685336b46f19dcc641a59271f3a";
+const PROD_CLIENT_ID = "72131a63bbf347de9186bfa6d422b601";
+
+const today = new Date();
+const twoDaysAgo = new Date(today).setDate(today.getDate() - 2);
+const oneYearAgo = new Date(today).setDate(today.getDate() - 365);
+
+const smdAuthParams = {
+  client_id: QA_CLIENT_ID,
+  redirect_uri: "https://www.isharefood.com/OAuthCallback",
+  response_type: "code",
+  login: "guest",
+  // scope: `MinAuthEndDate=${twoDaysAgo};PreferredAuthEndDate=${oneYearAgo}`
+};
+
+const withQuery = (query) => (url) => {
+  const params = Object.keys(query).reduce(
+    (acc, key, idx) => `${acc}${idx === 0 ? "?" : "&"}${key}=${query[key]}`,
+    ""
+  );
+  return `${url}${params}`;
+};
+
+app.get("/", (req, res) => {
+  const url = withQuery(smdAuthParams)(SMD_AUTH_BASE_URL);
+  // console.log(url);
+  res.redirect(url);
+});
+
+app.get("/OAuthCallback", async (req, res) => {
+  const headers = {
+    Authorization:
+      `Basic OWI3YzU2ODUzMzZiNDZmMTlkY2M2NDFhNTkyNzFmM2E6NmI3YzhlYWE4ZGYxNDRjOThlYTE3ZmFhOWNiZDllYWY=`,
+  };
+  const params = {
+    grant_type: "authorization_code",
+  };
+
+  const httpsAgent = new https.Agent({
+    cert: fs.readFileSync("ssl/certs/isharefood.com/certificate.crt"),
+    key: fs.readFileSync("ssl/private/isharefood.com/private.key")
+  });
+
+  const data = {
+    grant_type: "authorization_code",
+    code: req.query.code,
+    redirect_uri: "https://www.isharefood.com/OAuthCallback2",
+  };
+
+  const result = await axios.post(
+    withQuery(params)(`https://apiqa.pge.com/datacustodian/oauth/v2/token`),
+    data,
+    { data, httpsAgent, headers },
+  );
+  res.send(result.data);
+  // console.log(req.query.code);
+  // res.send("oAuthCallback");
+  // const base64String =
+  //   "Basic OWI3YzU2ODUzMzZiNDZmMTlkY2M2NDFhNTkyNzFmM2E6NmI3YzhlYWE4ZGYxNDRjOThlYTE3ZmFhOWNiZDllYWY=";
+
+
+
+  // const headers = {
+  //   "Content-Type": "application/json",
+  //   Authorization: base64String,
+  // };
+
+  // axios.post(`${PGE_API_BASE_URL}/oauth/v2/token`, data, {
+  //   headers,
+  // });
+});
+
+app.get('/OAuthCallback2', (req, res) => {
+  res.send('callback 2')
+});
+
+app.listen(port, (_) => {
+  console.log(`App Listening at http://localhost:${port}`);
+});
