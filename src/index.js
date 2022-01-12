210330df7e815b071fd115b5870e9510fe2e93b0
tomas.cormons <tcormons@gmail.com>
2022-01-12 15:30:51 -0800

    includes comment to document potential use case for data payload in body of POST request, currently working without it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const { default: axios } = require("axios");
-const https = require("https");
-const fs = require("fs");
-const express = require("express");
+const { default: axios } = require('axios');
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
 const app = express();
-const port = 3003;
+const port = 3004;
 
 const SMD_AUTH_BASE_URL = `https://sharemydataqa.pge.com/myAuthorization/`;
 const PGE_API_BASE_URL = `https://apiqa.pge.com`;
 
-const QA_CLIENT_ID = "9b7c5685336b46f19dcc641a59271f3a";
-const PROD_CLIENT_ID = "72131a63bbf347de9186bfa6d422b601";
+const QA_CLIENT_ID = '9b7c5685336b46f19dcc641a59271f3a';
+const PROD_CLIENT_ID = '72131a63bbf347de9186bfa6d422b601';
 
 const today = new Date();
 const twoDaysAgo = new Date(today).setDate(today.getDate() - 2);
@@ -17,64 +17,49 @@ const oneYearAgo = new Date(today).setDate(today.getDate() - 365);
 
 const smdAuthParams = {
   client_id: QA_CLIENT_ID,
-  redirect_uri: "https://www.isharefood.com/OAuthCallback",
-  response_type: "code",
-  login: "guest",
+  redirect_uri: 'https://www.isharefood.com/OAuthCallback',
+  response_type: 'code',
+  login: 'guest',
   // scope: `MinAuthEndDate=${twoDaysAgo};PreferredAuthEndDate=${oneYearAgo}`
 };
 
 const withQuery = (query) => (url) => {
   const params = Object.keys(query).reduce(
-    (acc, key, idx) => `${acc}${idx === 0 ? "?" : "&"}${key}=${query[key]}`,
-    ""
+    (acc, key, idx) => `${acc}${idx === 0 ? '?' : '&'}${key}=${query[key]}`,
+    ''
   );
   return `${url}${params}`;
 };
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const url = withQuery(smdAuthParams)(SMD_AUTH_BASE_URL);
   res.redirect(url);
 });
 
-app.get("/OAuthCallback", async (req, res, next) => {
+app.get('/OAuthCallback', async (req, res, next) => {
   const headers = {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     Authorization: `Basic OWI3YzU2ODUzMzZiNDZmMTlkY2M2NDFhNTkyNzFmM2E6NmI3YzhlYWE4ZGYxNDRjOThlYTE3ZmFhOWNiZDllYWY=`,
-  };
-  const params = {
-    grant_type: "authorization_code",
   };
 
   const httpsAgent = new https.Agent({
-    cert: fs.readFileSync("ssl/certs/isharefood.com/certificate.crt"),
-    key: fs.readFileSync("ssl/private/isharefood.com/private.key"),
+    cert: fs.readFileSync('ssl/certs/isharefood.com/certificate.crt'),
+    key: fs.readFileSync('ssl/private/isharefood.com/private.key'),
   });
 
   const data = {
-    grant_type: "authorization_code",
+    grant_type: 'authorization_code',
     code: req.query.code,
-    redirect_uri: "https://www.isharefood.com/OAuthCallback",
+    redirect_uri: 'https://www.isharefood.com/OAuthCallback',
   };
 
   const result = await axios.post(
     withQuery(data)(`https://apiqa.pge.com/datacustodian/oauth/v2/token`),
+    // TODO: data payload could be necessary arg, currently works as params above ^^^
     '',
     { httpsAgent, headers }
   );
   res.send(result.data);
-  // console.log(req.query.code);
-  // res.send("oAuthCallback");
-  // const base64String =
-  //   "Basic OWI3YzU2ODUzMzZiNDZmMTlkY2M2NDFhNTkyNzFmM2E6NmI3YzhlYWE4ZGYxNDRjOThlYTE3ZmFhOWNiZDllYWY=";
-
-  // const headers = {
-  //   "Content-Type": "application/json",
-  //   Authorization: base64String,
-  // };
-
-  // axios.post(`${PGE_API_BASE_URL}/oauth/v2/token`, data, {
-  //   headers,
-  // });
 });
 
 app.listen(port, (_) => {
