c52c3d5f8087b652d4f88d1f105c082c30a2e5fb
Chris Coker <chris.coker@verys.com>
2022-01-12 17:53:59 -0500

    successfully able to acquire an access_token form apiqa.pge.com

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { default: axios } = require("axios");
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const port = 3003;
+const qs = require("qs");
 
 const SMD_AUTH_BASE_URL = `https://sharemydataqa.pge.com/myAuthorization/`;
 const PGE_API_BASE_URL = `https://apiqa.pge.com`;
@@ -33,14 +34,13 @@ const withQuery = (query) => (url) => {
 
 app.get("/", (req, res) => {
   const url = withQuery(smdAuthParams)(SMD_AUTH_BASE_URL);
-  // console.log(url);
   res.redirect(url);
 });
 
-app.get("/OAuthCallback", async (req, res) => {
+app.get("/OAuthCallback", async (req, res, next) => {
   const headers = {
-    Authorization:
-      `Basic <SECRET_STRING>`,
+    "Content-Type": "application/json",
+    Authorization: `Basic <SECRET_STRING>`,
   };
   const params = {
     grant_type: "authorization_code",
@@ -48,26 +48,25 @@ app.get("/OAuthCallback", async (req, res) => {
 
   const httpsAgent = new https.Agent({
     cert: fs.readFileSync("ssl/certs/isharefood.com/certificate.crt"),
-    key: fs.readFileSync("ssl/private/isharefood.com/private.key")
+    key: fs.readFileSync("ssl/private/isharefood.com/private.key"),
   });
 
   const data = {
     grant_type: "authorization_code",
     code: req.query.code,
-    redirect_uri: "https://www.isharefood.com/OAuthCallback2",
+    redirect_uri: "https://www.isharefood.com/OAuthCallback",
   };
 
+  // console.log(qs.stringify(data));
+
   const result = await axios.post(
-    withQuery(params)(`https://apiqa.pge.com/datacustodian/oauth/v2/token`),
-    data,
-    { data, httpsAgent, headers },
+    withQuery(data)(`https://apiqa.pge.com/datacustodian/oauth/v2/token`),
+    '',
+    { httpsAgent, headers }
   );
   res.send(result.data);
 });
 
-app.get('/OAuthCallback2', (req, res) => {
-  res.send('callback 2')
-});
 
 app.listen(port, (_) => {
   console.log(`App Listening at http://localhost:${port}`);
