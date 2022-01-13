fceb956afc75503f787012bb04cafa591276a010
Chris Coker <chris.coker@verys.com>
2022-01-13 10:05:09 -0500

    cleaned up code a bit

--- src/index.js
+++ src/index.js
@@ -4,32 +4,33 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const port = 3003;
+const qs = require("qs");
 
 const SMD_AUTH_BASE_URL = `https://sharemydataqa.pge.com/myAuthorization/`;
 const PGE_API_BASE_URL = `https://apiqa.pge.com`;
 
 const QA_CLIENT_ID = "<SECRET_STRING>";
+const QA_CLIENT_SECRET = "<SECRET_STRING>";
 const PROD_CLIENT_ID = "<SECRET_STRING>";
+
+const CLIENT_ID = QA_CLIENT_ID;
+const CLIENT_SECRET = QA_CLIENT_SECRET;
 
 const today = new Date();
 const twoDaysAgo = new Date(today).setDate(today.getDate() - 2);
 const oneYearAgo = new Date(today).setDate(today.getDate() - 365);
 
 const smdAuthParams = {
-  client_id: QA_CLIENT_ID,
+  client_id: CLIENT_ID,
   redirect_uri: "https://www.isharefood.com/OAuthCallback",
   response_type: "code",
-  login: "guest",
-  // scope: `MinAuthEndDate=${twoDaysAgo};PreferredAuthEndDate=${oneYearAgo}`
+  login: "guest"
 };
 
-const withQuery = (query) => (url) => {
-  const params = Object.keys(query).reduce(
-    (acc, key, idx) => `${acc}${idx === 0 ? "?" : "&"}${key}=${query[key]}`,
-    ""
-  );
-  return `${url}${params}`;
-};
+const withQuery = (params) => (url) =>
+  `${url}${Object.keys(params).length ? "?" : ""}${qs.stringify(params)}`;
+
+const encode64 = (str) => Buffer.from(str, "utf-8").toString("base64");
 
 app.get("/", (req, res) => {
   const url = withQuery(smdAuthParams)(SMD_AUTH_BASE_URL);
@@ -39,10 +40,7 @@ app.get("/", (req, res) => {
 app.get("/OAuthCallback", async (req, res, next) => {
   const headers = {
     "Content-Type": "application/json",
-    Authorization: `Basic <SECRET_STRING>`,
-  };
-  const params = {
-    grant_type: "authorization_code",
+    Authorization: `Basic ${encode64(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
   };
 
   const httpsAgent = new https.Agent({
@@ -58,7 +56,7 @@ app.get("/OAuthCallback", async (req, res, next) => {
 
   const result = await axios.post(
     withQuery(data)(`https://apiqa.pge.com/datacustodian/oauth/v2/token`),
-    '',
+    "",
     { httpsAgent, headers }
   );
   res.send(result.data);
